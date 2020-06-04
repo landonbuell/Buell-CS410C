05bee2b4301cd1153eb77b50aeff8afa9813125d
landonbuell <lhb1007@wildcats.unh.edu>
2020-06-03 22:34:02 -0400

    3 June 2020
    - Initiated repo
    - Added programming assignments

---
+++ Program0/Program0_MAIN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program0
+{
+    class Program0_MAIN
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
