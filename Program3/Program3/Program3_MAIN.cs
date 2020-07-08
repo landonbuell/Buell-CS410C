0e02dbc88616620df277b9401f7ac96e81b927e8
landonbuell <lhb1007@wildcats.unh.edu>
2020-07-07 21:57:32 -0400

    7 July 2020 (1)
    - progress on program 3

---
+++ Program3/Program3/Program3_MAIN.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Landon Buell
+ * CS-410C
+ * Program3 - Main
+ * 7 July 2020
+ */
+
+using System;
+
+namespace Program3
+{
+    class Program3_MAIN
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                // Accept User Input
+                Console.Write("Please Input and integer between 1 and 1000:");
+                try
+                {
+                    int input = Convert.ToInt32(Console.ReadLine());
+
+                    // Test User Input:
+                    if (input < 1 ^ input > 1000)
+                    {
+                        Console.WriteLine("\n\tERROR! - Integer out of range, Please try again\n");
+                    }
+                    else if (input == 1000)
+                    {
+                        // Terminate program if input is 1000
+                        break;
+                    }
+                    else
+                    {
+                        // Create Object and Run tests
+                        var sequences = new MathSequences(input);
+                    }
+                }
+                catch
+                {
+                    // Exception, Probably invalid user type
+                    Console.WriteLine("\n\tERROR! - Could not convert to integer, Please try again\n");
+                }
+                
+            }
+        }
+    }
+}
---
+++ Program3/Program3/Program3_Utilities.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Landon Buell
+ * CS-410C
+ * Program3 - Utilities
+ * 7 July 2020
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Program3
+{
+    class MathSequences
+    {
+        public int value;
+        public List<int> Fib_nums;
+
+        public MathSequences(int user_input)
+        {
+            // Class Constructor
+            this.value = user_input;
+        }
+
+        public List RunFibbonacci()
+        {
+
+        }
+
+        public int Fibbonacci (int N)
+        {
+               
+            if (N == 0) { return 0; }
+            else if (N == 1) { return 1; }
+            else { return Fibbonacci(N - 1) + Fibbonacci(N + 1); }
+        }
+
+    }
+}
